b5c7290282f95f97d8305f562de1b7f38c5328ce
riyadmahmudkhan <mdriyadhossain356@gmail.com>
2023-03-26 01:53:11 +0600

    finish project

--- Components/Countries.js
+++ Components/Countries.js
@@ -1,20 +1,33 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Country from './Country';
 
 export default function Countries() {
     const [countries, setCountries] = useState([]);
+    const [seacred, setSeacred] = useState([]);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
-            .then(data => setCountries(data))
+            .then(data => {
+                setSeacred(data)
+                setCountries(data)
+            })
     }, [])
+
+    const handleSearch = text => {
+        const filter = countries.filter(country => country.name.common.includes(text));
+        setSeacred(filter);
+    }
     return (
         <View>
-            <Text>Countries: {countries.length}</Text>
+            <Text style={styles.header}>Countries: {countries.length}</Text>
+            <TextInput
+                onChangeText={handleSearch}
+                style={styles.input}
+            ></TextInput>
             <ScrollView>
                 {
-                    countries.map(country => <Country
+                    seacred.map(country => <Country
                         // key={}
                         country={country}
                     ></Country>)
@@ -23,3 +36,17 @@ export default function Countries() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        marginTop: 50,
+        fontSize: 40,
+        color: 'red'
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    }
+});
